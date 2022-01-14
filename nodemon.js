1d62e9bf1756832fe657f7a981a04c69c82141b2
robertogriel <roberto@griel.com.br>
2022-01-14 15:12:19 -0300

    Instalando e testando o Nodemon

---
+++ nodemon.js
@@ -0,0 +1,12 @@
+/**
+ * Instalando o nodemon
+ * npm install -g nodemon (Para instalação global)
+ * 
+ * Rodando o nodemon:
+ * nodemon arquivo.js
+ */
+
+const carro = "Fusca";
+
+console.log(`Carro: ${carro}`);
+
