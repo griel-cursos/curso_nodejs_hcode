5ca51268b8f0a783abdd15cf5306984413584da9
robertogriel <roberto@griel.com.br>
2022-01-18 15:00:24 -0300

    Atualizando a aula de eventos

--- events/events.js
+++ events/events.js
@@ -1,4 +1,4 @@
-const EventEmitter = require('events');
+const {EventEmitter} = require('events');
 
 //Criando uma classe de exemplo que extende do EventEmitter
 class Evento extends EventEmitter {}
@@ -8,8 +8,14 @@ const meuEvento = new Evento();
 
 //Crio um evento com o nome seguranca e passo os argumentos que eu desejo
 meuEvento.on('seguranca', (x,y)=>{
-    console.log(`Evento de segurança: ${x} e ${y}`)
+    console.log(`Evento de segurança: ${x} ${y}`)
 });
 
 //Invoco um método, chamando o evento seguranca com nome de usuário e a ação, como exemplo
 meuEvento.emit('seguranca','Roberto','Alterou o salário');
+
+meuEvento.on('encerrar',(dados)=>{
+    console.log('Assinante:' + dados);
+});
+
+meuEvento.emit('encerrar','Encerrando a execução')
