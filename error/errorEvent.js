66918746c20969eddccf4296a348dde3d6f10db9
robertogriel <roberto@griel.com.br>
2022-01-18 15:53:01 -0300

    Tratando erros com eventos

---
+++ error/errorEvent.js
@@ -0,0 +1,19 @@
+const { EventEmitter } = require('events');
+
+const emitter = new EventEmitter();
+
+const validaObjeto = (a)=>{
+    if (typeof a !== 'object') {
+        emitter.emit('error', new Error('Objeto inválido'));
+    } else {
+        console.log('Objeto válido');
+    }
+}
+
+emitter.addListener('error', (err)=>{
+    console.log(`Evento: ${err.message}`);
+})
+
+let dados = {name:'Roberto',course:'NodeJS'};
+
+validaObjeto(123)
